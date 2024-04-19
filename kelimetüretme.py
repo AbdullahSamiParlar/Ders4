5997626509371e6a5077ca452b8da02c70ca494b
AbdullahSamiParlar <164669555+AbdullahSamiParlar@users.noreply.github.com>
2024-04-19 19:20:40 +0300

    Create kelimetüretme.py

---
+++ kelimetüretme.py
@@ -0,0 +1,73 @@
+#Bu Fonksiyondun Asıl Kod Geliştirilmiş Olan
+
+kelimeler=["ağaç", "aşk", "ağlamak", "anlamak", "araba", "arkadaş", "akıl", "ayna", "anne", "aşure", "ananas", "armut", "adam", "almak", "ayakkabı", "ay", "aslan", "at", "ahır", "ayva", "arı"
+"balık", "baba", "başarı", "bilgisayar", "biber", "bakkal", "bal", "bayram", "bakmak", "bıçak", "boya", "buzdolabı", "banka", "bisiklet", "babam", "babası", "bilet", "baklava", "böcek", "bot"
+"çocuk", "çanta", "çorap", "çay", "çocuklu", "çilek", "çınar", "çimen", "çiçek", "çikolata", "çöp", "çalışmak", "çıkmak", "çalmak", "çetin", "çevirmek", "çamaşır", "çökmek", "çarpmak", "çevre"
+"dev", "dost", "düşmek", "düşünmek", "dürbün", "duman", "dövmek", "düğün", "dönem", "dönüşmek", "döviz", "dil", "dolap", "dalgın", "dolmuş", "dalga", "değişmek", "deri", "dua", "duruş"
+"elma", "ev", "el", "et", "eşya", "eğitim", "eğmek", "eşmek", "eşmek", "eş", "ebeveyn", "eylem", "eğlenmek", "evlenmek", "etek", "elbise", "enerji", "erkek", "eylemek", "elbise"
+"fırın", "fener", "fil", "futbol", "fındık", "farklı", "fotograf", "fotoğraf", "fena", "fırsat", "fermuar", "fabrika", "fare", "fatura", "fakir", "fırça", "fırın", "fırın", "fırlatmak", "fırlamak"
+"güzel", "güneş", "gün", "göz", "görmek", "gazete", "gaz", "göbek", "gölge", "göğüs", "giriş", "güçlü", "gülmek", "gelmek", "göl", "göndermek", "gitmek", "gezmek", "giriş", "gezmek"
+"hızlı", "hediye", "hayvan", "hayat", "havlu", "halı", "hastane", "haklı", "hal", "harita", "havuç", "hazır", "havuz", "hava", "hırsız", "hayal", "hükümet", "hak", "hakim", "hakikat"
+"ışık", "içmek", "iş", "iğne", "insan", "içmek", "içki", "ikiz", "içmek", "içgüdü", "içeri", "iyi", "içeri", "için", "iç", "iğne", "iğne", "isim", "içgüdü", "iklim"
+"jandarma", "jilet", "japon", "jöle", "joystick", "jet", "jazz", "jigolo", "judo", "jön", "jüri", "jelatin", "jübile", "jöntürk", "jeoloji", "jartiyer", "jüpiter", "jeneratör", "jak", "jenerik"
+"kahve", "kitap", "kalp", "kedi", "kar", "kalem", "kırmızı", "kısa", "kış", "kapı", "kablo", "kuzu", "karanfil", "kaynak", "kale", "kadeh", "kabuk", "kablosuz", "kız", "kurt"
+"limon", "lamba", "lise", "lazanya", "lanet", "limonata", "lavabo", "lamba", "lale", "leke", "lodos", "lavanta", "laboratuvar", "lokanta", "limuzin", "lider", "lokomotif", "lahana", "limonlu", "lazer"
+"mağaza", "masa", "meyve", "meyhane", "marul", "maymun", "martı", "masa", "meyve", "mısır", "makyaj", "muz", "müzik", "manzara", "mum", "mart", "makine", "mendil", "mısır", "müşteri"
+"nar", "nane", "nisan", "nohut", "neyse", "neşe", "nargile", "nesne", "nefes", "nevresim", "normal", "nerede", "neden", "nikah", "nakit", "neden", "nefis", "nispet", "nokta", "nişan"
+"özür", "öğrenci", "özlem", "öyle", "ölüm", "ödemek", "öfke", "ölçmek", "örgü", "özgür", "ölçü", "öğle", "önemli", "önem", "örnek", "özel", "ördek", "öz", "özdeşlik", "öksürmek"
+"papatya", "pilav", "paket", "pazar", "pencere", "para", "pasta", "patates", "pilot", "piknik", "pikap", "paket", "portakal", "portre", "polis", "prenses", "pembe", "papa" , "papatya", "pilav", "paket", "pazar", "pencere", "para", "pasta", "patates", "pilot", "piknik", "pikap", "paket", "portakal", "portre", "polis", "prenses", "pembe", "papağan", "paten", "patlak"
+"rüya", "renk", "radyo", "rahat", "rüzgar", "rakı", "rağmen", "rakam", "rahmet", "ramazan", "rakip", "resim", "rengarenk", "reçel", "rağbet", "ramp", "rekabet", "reklam", "romantik", "radyasyon"
+"sandalye", "saat", "sabah", "sarı", "soba", "süt", "sokak", "sinek", "sorun", "sıcak", "söylemek", "sepet", "soğuk", "sıra", "salata", "su", "soyadı", "saklamak", "sinek", "sucuk"
+"tahta", "tavuk", "telefon", "tencere", "tabak", "taraf", "tane", "top", "televizyon", "tuz", "temizlik", "türk", "tavla", "tüketmek", "tarih", "tartmak", "taze", "tombala", "teknoloji", "timsah"
+"üzüm", "üst", "ücretsiz", "üzgün", "üniversite", "üçgen", "üyelik", "ün", "üzer", "ülke", "üzüm", "ülkü", "ünlü", "ülser", "üzülmek", "üşümek", "üzgün", "üzüntü", "üzücü", "üstelik"
+"vapur", "vermek", "vatandaş", "vergi", "vakit", "vazo", "vazgeçmek", "vitrin", "vitamin", "voleybol", "vadi", "vurmak", "vızıltı", "valiz", "veranda", "vermek", "vahşi", "vanilya", "vazife", "vali"
+"yemek", "yüz", "yapmak", "yanmak", "yıldız", "yatak", "yol", "yakmak", "yazmak", "yüzme", "yağ", "yok", "yarı", "yanlış", "yalan", "yönetmek", "yanlı", "yolculuk", "yakın", "yorgun"
+"zaman", "zeytin", "zeytin", "zeybek", "zil", "zor", "zehir", "zeytinyağı", "zarar", "zeka", "zengin", "zemin", "zıplamak", "zorlu", "zorunlu", "zeka", "zaman", "zorunda", "zengin", "zehirli"
+]
+
+sonharf="a"
+kelimelog=[]
+
+Open=True
+def kelimetüret(user, m, score):
+    global sonharf
+    
+
+    
+    if Open==True:
+        kelime = m
+        kelimeninsonharfi = kelime[-1]
+        kelimeninilkharfi = kelime[0]
+        print("son",sonharf)
+        print("kson",kelimeninsonharfi)
+        print("ilk",kelimeninilkharfi)
+        
+        if kelime in kelimelog:
+            return ["Bu kelime zaten kullanılmış",score]
+        elif kelime in kelimeler and kelimeninilkharfi == sonharf:
+            sonharf = kelimeninsonharfi
+            kelimelog.append(kelime)
+            print("yson",sonharf)
+            score+=1
+            return [f"{user}! Doğru cevap! +1 puan",score]
+        
+        elif kelime not in kelimeler and kelimeninilkharfi == sonharf:
+            return [f"{user}! Böyle bir kelime yok! 0 puan",score]
+        
+        elif kelime in kelimeler and kelimeninilkharfi != sonharf:
+            score-=1
+            return [f"{user}! Son kelimenin son harfi kelimenin ilk harfi olmalı! -1 ceza!",score]
+            
+
+        elif kelime not in kelimeler and kelimeninilkharfi != sonharf:
+            score-=2
+            return [f"{user}! Ne kelime ne harf doğru!! -2  ceza!",score]
+            
+    
+
+
+
+
+
+
+
